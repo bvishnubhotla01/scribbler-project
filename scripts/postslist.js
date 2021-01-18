9c7031c3730cac05a720cc16a178524b4b85b6a3
Balaji Vishnubhotla <balaji.vishnubhotla@gmail.com>
2021-01-18 21:49:18 +0530

    Create and edit Posts Completed

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -4,6 +4,8 @@ function removeCard(card) {
 }
 
 function openPost(author, heading, content) {
-        const url = `./post.html`;
+        const url = `./post.html?heading=${encodeURIComponent(heading.innerText)}&author=${encodeURIComponent(
+                author.innerText
+        )}&content=${encodeURIComponent(content.innerText)}`;
         window.location.href = url;
 }
